4625cc38c0eb93520ed08fbc37a6adad6155c366
xmh0511 <970252187@qq.com>
2022-08-10 17:51:50 +0800

    add router functionalities

--- execution/src/main.rs
+++ execution/src/main.rs
@@ -4,11 +4,6 @@ use http_server::EndPoint;
 
 
 fn main() {
-	// let http_server = http_server::HttpServer::create(end_point!(0.0.0.0:8080), 10);
-	// http_server.run();
-	let r = [(1,"ab"),(2,"ab")];
-	let d = r.binary_search_by_key(&2, |&(a,b)|{
-		 a
-	});
-	println!("{:?}",d);
+	let http_server = http_server::HttpServer::create(end_point!(0.0.0.0:8080), 10);
+	http_server.run();
 }
